9f71116ca756f1cc68e3f3fd2cf9ce72c4604110
mukesh786yadav <1032190391@tcetmumbai.in>
2023-10-08 22:47:33 +0530

    new file added

--- server/dist/index.js
+++ server/dist/index.js
@@ -2,22 +2,35 @@
 
 var _express = _interopRequireDefault(require("express"));
 var _dotenv = _interopRequireDefault(require("dotenv"));
+var _passport = _interopRequireDefault(require("passport"));
+var _route = _interopRequireDefault(require("./config/route.config"));
 var _connection = _interopRequireDefault(require("./database/connection.js"));
-var _index = _interopRequireDefault(require("./api/auth/index.js"));
+var _auth = _interopRequireDefault(require("./api/auth"));
+var _food = _interopRequireDefault(require("./api/food"));
+var _restaurant = _interopRequireDefault(require("./api/restaurant"));
+var _user = _interopRequireDefault(require("./api/user"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+//importing config file
+
 //database connection 
 
-//importing Auth router
+//importing All router
 
 _dotenv.default.config();
 const zomato = (0, _express.default)();
+//adding additional configration
+(0, _route.default)(_passport.default);
 zomato.use(_express.default.json());
+zomato.use(_passport.default.initialize());
 zomato.get("/", (req, res) => {
   res.json({
     message: "Hello world"
   });
 });
-zomato.post("/auth", _index.default);
+zomato.use("/auth", _auth.default);
+zomato.use("/food", _food.default);
+zomato.use("/restaurant", _restaurant.default);
+zomato.use("/user", _user.default);
 zomato.listen(4000, () => {
   (0, _connection.default)().then(() => {
     console.log("Server is running on port 4000");
--- server/index.js
+++ server/index.js
@@ -1,18 +1,45 @@
 import  express  from "express";
 import dotenv from "dotenv";
+import passport from "passport";
+import session from "express-session";
+
+//importing config file
+import privateRouteConfig from "./config/route.config"
+
 //database connection 
 import ConnetDB from "./database/connection.js";
-//importing Auth router
-import  Auth from "./api/auth"
+
+//importing All router
+import  Auth from "./api/auth";
+import Food from  "./api/food";
+import Restaurant from  "./api/restaurant";
+import User from  "./api/user";
+import Menu from "./api/menu";
+import Order from "./api/order";
+import Review from "./api/review";
+import Image from "./api/image";
  dotenv.config();
 const zomato =express();
+//adding additional configration
+privateRouteConfig(passport);
 
 zomato.use(express.json());
+zomato.use(session({ secret: process.env.JWTSECRET }));
+zomato.use(passport.initialize());
 
 zomato.get("/",(req,res)=>{
     res.json({message:"Hello world"})
 })
+// all routes ...
 zomato.use("/auth",Auth);
+zomato.use("/food", Food);
+zomato.use("/restaurant", Restaurant);
+zomato.use("/user", User);
+zomato.use("/menu", Menu);
+zomato.use("/order", Order);
+zomato.use("/review", Review);
+zomato.use("/image", Image);
+
 zomato.listen(4000, ()=>{
     ConnetDB()
     .then( ()=>{
